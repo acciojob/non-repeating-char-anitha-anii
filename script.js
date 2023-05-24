7adf997b2708ca47ec0838d9a2b0a37b3dd13fd1
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-24 15:23:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+ 
+  function findFirstNonRepeatedChar() {
+  const input = prompt("Enter a string");
+  
+  for (let i = 0; i < input.length; i++) {
+    let char = input.charAt(i);
+    
+    if (input.indexOf(char) === input.lastIndexOf(char)) {
+      alert(char);
+      return;
+    }
+  }
+  }
+findFirstNonRepeatedChar()
+
